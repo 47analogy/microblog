830a129d6bbc230be791975fa04cf2fb47cadd45
analogy47 <actweed47@gmail.com>
2018-11-22 12:42:23 -0500

    modify blog posts route add base template add homepage template

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,20 @@ app = Flask(__name__)
 @app.route('/index')
 # view functions are similar to handlers
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Hulk'}
+    posts = [
+
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Spider Man'},
+            'body': 'You can not beat me Hulk'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
